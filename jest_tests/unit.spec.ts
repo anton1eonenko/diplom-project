da5cacd0dded1b56dfc5908052818fca5eccc61e
Anton <anton1eonenko@mail.ru>
2023-02-08 19:38:26 +0300

    added units

--- jest_tests/unit.spec.ts
+++ jest_tests/unit.spec.ts
@@ -1,26 +1,26 @@
 import { RegForm } from "../registrationForm"
-describe ('FirstName', () => {
-    test ('+1', () => {
+describe ('Name', () => {
+    test ('liquid name', () => {
         let expected = 'Anton'
         let actual = new RegForm().FirstName('Anton')
         expect(actual).toBe(expected)
     })
-    test ('+2', () => {
+    test ('liquid name', () => {
         let expected = 'anton'
         let actual = new RegForm().FirstName('anton')
         expect(actual).toBe(expected)
     })
-    test ('+3', () => {
+    test ('liquid name', () => {
         let expected = 'Антон'
         let actual = new RegForm().FirstName('Антон')
         expect(actual).toBe(expected)
     })
-    test ('-1', () =>{
+    test ('illiquid name', () =>{
         let expected = 'Anton'
         let actual = new RegForm().FirstName(false)
         expect(actual).toBe(expected)
     })
-    test ('-2', () =>{
+    test ('illiquid name', () =>{
         let expected = 'Anton'
         let actual = new RegForm().FirstName(123)
         expect(actual).toBe(expected)
@@ -28,27 +28,27 @@ describe ('FirstName', () => {
 })
 
 describe ('LastName', () => {
-    test ('+1', () => {
+    test ('liquid Lastname', () => {
         let expected = 'Leonenko'
         let actual = new RegForm().FirstName('Leonenko')
         expect(actual).toBe(expected)
     })
-    test ('+2', () => {
+    test ('liquid Lastname', () => {
         let expected = 'leonenko'
         let actual = new RegForm().FirstName('leonenko')
         expect(actual).toBe(expected)
     })
-    test ('+3', () => {
+    test ('liquid Lastname', () => {
         let expected = 'Леоненко'
         let actual = new RegForm().FirstName('Леоненко')
         expect(actual).toBe(expected)
     })
-    test ('-1', () =>{
+    test ('illiquid Lastname', () =>{
         let expected = 'Leonenko'
         let actual = new RegForm().FirstName(true)
         expect(actual).toBe(expected)
     })
-    test ('-2', () =>{
+    test ('illiquid Lastname', () =>{
         let expected = 'Leonenko'
         let actual = new RegForm().FirstName(123)
         expect(actual).toBe(expected)
@@ -56,27 +56,27 @@ describe ('LastName', () => {
 })
 
 describe ('Country', () => {
-    test ('+1', () => {
+    test ('liquid Country', () => {
         let expected = 'Belarus'
         let actual = new RegForm().Country('Belarus')
         expect(actual).toBe(expected)
     })
-    test ('+2', () => {
+    test ('liquid Country', () => {
         let expected = 'Belarus'
         let actual = new RegForm().Country('USA')
         expect(actual).toBe(expected)
     })
-    test ('-1', () => {
+    test ('illiquid Country', () => {
         let expected = 'Belarus'
         let actual = new RegForm().Country(123)
         expect(actual).toBe(expected)
     })
-    test ('-2', () => {
+    test ('illiquid Country', () => {
         let expected = 'Belarus'
         let actual = new RegForm().Country('')
         expect(actual).toBe(expected)
     })
-    test ('-3', () => {
+    test ('illiquid Country', () => {
         let expected = 'Belarus'
         let actual = new RegForm().Country('belarus')
         expect(actual).toBe(expected)
@@ -84,32 +84,32 @@ describe ('Country', () => {
 })
 
 describe ('EMail', () => {
-    test ('+1', () => {
+    test ('liquid EMail', () => {
         let expected = 'Ok'
         let actual = new RegForm().EMail('antonleon@mail.com')
         expect(actual).toBe(expected)
     })
-    test ('+2', () => {
+    test ('liquid EMail', () => {
         let expected = 'Ok'
         let actual = new RegForm().EMail('vasyapup@mail.ru')
         expect(actual).toBe(expected)
     })
-    test ('+3', () => {
+    test ('liquid EMail', () => {
         let expected = 'Данный адрес уже занят'
         let actual = new RegForm().EMail('anton@tut.by')
         expect(actual).toBe(expected)
     })
-    test ('-1', () => {
+    test ('illiquid EMail', () => {
         let expected = 'Проверьте введенные данные'
         let actual = new RegForm().EMail('anton@@tut.by')
         expect(actual).toBe(expected)
     })
-    test ('-2', () => {
+    test ('illiquid EMail', () => {
         let expected = 'Проверьте введенные данные'
         let actual = new RegForm().EMail('antontut.by')
         expect(actual).toBe(expected)
     })
-    test ('-3', () => {
+    test ('illiquid EMail', () => {
         let expected = 'Проверьте введенные данные'
         let actual = new RegForm().EMail('anton@tutby')
         expect(actual).toBe(expected)
@@ -117,27 +117,27 @@ describe ('EMail', () => {
 })
 
 describe ('PhoneNumber', () => {
-    test ('+1', () => {
+    test ('liquid PhoneNumber', () => {
         let expected = 'Телефон верный'
         let actual = new RegForm().PhoneNumber('+375(29)374-08-28')
         expect(actual).toBe(expected)
     })
-    test ('+2', () => {
+    test ('liquid PhoneNumber', () => {
         let expected = 'Телефон верный'
         let actual = new RegForm().PhoneNumber('+375(33)374-85-28')
         expect(actual).toBe(expected)
     })
-    test ('-1', () => {
+    test ('illiquid PhoneNumber', () => {
         let expected = 'Телефон неверный, введите в формате +375(**)***-**-**'
         let actual = new RegForm().PhoneNumber('+375(293)374-08-28')
         expect(actual).toBe(expected)
     })
-    test ('-2', () => {
+    test ('illiquid PhoneNumber', () => {
         let expected = 'Телефон неверный, введите в формате +375(**)***-**-**'
         let actual = new RegForm().PhoneNumber('+375(29)374-08-28-82')
         expect(actual).toBe(expected)
     })
-    test ('-3', () => {
+    test ('illiquid PhoneNumber', () => {
         let expected = 'Телефон неверный, введите в формате +375(**)***-**-**'
         let actual = new RegForm().PhoneNumber('+37529374-08-28')
         expect(actual).toBe(expected)
@@ -145,22 +145,22 @@ describe ('PhoneNumber', () => {
 })
 
 describe ('Gender', () => {
-    test ('+1', () => {
+    test ('liquid Gender', () => {
         let expected = 'Ok'
         let actual = new RegForm().Gender('Male')
         expect(actual).toBe(expected)
     })
-    test ('+2', () => {
+    test ('liquid Gender', () => {
         let expected = 'Ok'
         let actual = new RegForm().Gender('Female')
         expect(actual).toBe(expected)
     })
-    test ('-1', () => {
+    test ('illiquid Gender', () => {
         let expected = 'Error'
         let actual = new RegForm().Gender('-')
         expect(actual).toBe(expected)
     })
-    test ('-2', () => {
+    test ('illiquid Gender', () => {
         let expected = 'Error'
         let actual = new RegForm().Gender('')
         expect(actual).toBe(expected)
